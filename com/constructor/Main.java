7387fa170e2076c7a3424aed2910453dca79ca25
Владислав <112858191+DeInJiI@users.noreply.github.com>
2023-03-26 19:03:52 +0300

    Delete Main.java

--- com/constructor/Main.java
+++
@@ -1,9 +0,0 @@
-package com.constructor;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Transport bmw = new Transport(250, 3000, "Blue", new byte[]{0, 0, 0});
-        Transport niva = new Transport(2500, new byte[]{3, 2, 0});
-    }
-}
